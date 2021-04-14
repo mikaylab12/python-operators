a99701993c360cd59793cab314c49f0a78cb7c51
Your Name <mikaylabeelders@gmail.com>
2021-04-14 16:30:12 +0200

    first commit

---
+++ operators.py
@@ -0,0 +1,29 @@
+#Task (Extra Excercise)
+#Use the two given sides of a triangle to detrermine the third side : 3 , 4
+
+""" AC = math.sqrt(AB**2 + BC**2)  - right-angled triangle
+AC = math.sqrt(4**2 + 3**2)"""
+import math
+
+AB = 4
+BC = 3
+AC =math.sqrt(4**2 + 3**2)
+print(AC)
+
+#Area of right-angled triangle
+#Area of right-angled triangle = 0.5 * base * height
+Area = 0.5 * 3 * 5
+print(Area)
+
+#Display in binary (7.5)
+int = 7.5
+binary = 0111.1
+print(" 7.5 in binary is 0111.1")
+
+#Give smallest number in list
+my_numbers = [2,56,12,67,1000, 32,89,29,44,39,200,11,21]
+print(min (my_numbers))
+#Give largest number in list
+print((max(my_numbers)))
+
+
